8b0330ea00bf816a1519e1573931763263c2b171
sarival <sarival@sarival.com.br>
2020-05-06 14:46:44 -0300

    Finalizado capitulo 16

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -4,9 +4,19 @@ import { CadastroComponent	}	from	'./modules/cadastro/cadastro.component';
 import { CaixaDeEntradaComponent } from './modules/caixa-de-entrada/caixa-de-entrada/caixa-de-entrada.component';
 
 const	rotas:	Routes	=	[
-	{path:	'',	component:	LoginComponent},
-	{path:	'inbox',	component:	CaixaDeEntradaComponent	},
-	{path:	'cadastro',	component:	CadastroComponent}
+	{	path:	'login',	component:	LoginComponent	},
+	{	path:	'inbox',	component:	CaixaDeEntradaComponent	},
+	{	path:	'cadastro',	component:	CadastroComponent	},
+	{
+			path:	'',
+			pathMatch:	'full',
+			redirectTo:	'inbox'
+	},
+	{
+			path:	'**',
+			pathMatch:	'full',
+			redirectTo:	'login'
+	}
 ]
 
 export const ModuloRoteamento = RouterModule.forRoot(rotas);
